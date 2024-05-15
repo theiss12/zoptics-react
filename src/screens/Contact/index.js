922ca8aba227db4393c241e51ad21bd10b567633
Tamas Zeiss <tamas.zeiss@gmail.com>
2024-05-15 09:02:38 +0200

    contact style final

--- src/screens/Contact/index.js
+++ src/screens/Contact/index.js
@@ -1,17 +1,23 @@
 import "./style.scss";
 
 function Contact() {
+    const copyContents = (clickEvent) => {
+        const contents = clickEvent.target.parentElement.innerHTML;
+        const copyText = contents.substring(0, contents.indexOf("<button"));
+        window.navigator.clipboard.writeText(copyText);
+    }
+
     return (
         <section className="screen-contact">
             <div className="container">
                 <iframe 
                     className="contact__map"
                     // width="100%" 
-                    height="600" 
+                    // height="600" 
                     frameBorder="0" 
-                    scrolling="no" 
-                    marginHeight="0" 
-                    marginWidth="0" 
+                    // scrolling="no" 
+                    // marginHeight="0" 
+                    // marginWidth="0" 
                     src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Sopron+(Zoptics)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
                 >
                     {/* <a href="https://www.gps.ie/">gps tracker sport</a> */}
@@ -19,10 +25,17 @@ function Contact() {
                 <div className="contact__wrapper">
                     <h2 className="contact__headline">Elérhetőség</h2>
                     <div className="contact__details">
-                        <p className="contact__details-label">Cégnév:</p><p className="contact__details-info">Zoptics Kft.</p>
-                        <p className="contact__details-label">Telefonszám:</p><a className="contact__details-info" href="tel:+4733378901">+47 333 78 901</a>
-                        <p className="contact__details-label">Email:</p><a className="contact__details-info" href="mailto:tamas.zeiss@gmail.com">tamas.zeiss@gmail.com</a>
-                        <p className="contact__details-label">Cím:</p><p className="contact__details-info">Túl Az Operencián 2</p>
+                        <p className="contact__details-label">Cégnév:</p>
+                        <p className="contact__details-info">Zoptics Kft.<button className="copy-button" onClick={copyContents}>📄</button></p>
+
+                        <p className="contact__details-label">Telefonszám:</p>
+                        <a className="contact__details-info" href="tel:+36012345678">+36 01 234 5678</a>
+
+                        <p className="contact__details-label">Email:</p>
+                        <a className="contact__details-info" href="mailto:tamas.zeiss@gmail.com">tamas.zeiss@gmail.com</a>
+                        
+                        <p className="contact__details-label">Cím:</p>
+                        <p className="contact__details-info">Túl Az Operencián 2<button className="copy-button" onClick={copyContents}>📄</button></p>
                     </div>
                 </div>
             </div>
